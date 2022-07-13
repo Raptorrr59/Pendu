0bfce8088f919eb0a10de6d12039c84d85806956
karma akabane <damien.birembaut@epitech.eu>
2022-07-13 17:05:22 +0200

    New Update

--- pendu/src/App.tsx
+++ pendu/src/App.tsx
@@ -1,12 +1,14 @@
 import React, {useEffect, useState} from 'react';
-import { setSyntheticLeadingComments } from 'typescript';
+import {
+  Link
+} from "react-router-dom";
 import './App.css';
-import './Keyboard.css';
+import './Keyboard.scss';
 import WORD_LIST from './Word';
-//import Keyboard from './Keyboard';
 
 function App() {
   const [count, setCount] = useState(10);
+  const [turn, setTurn] = useState(1);
   const [word] = useState(WORD_LIST[Math.floor(Math.random() * WORD_LIST.length)]);
   const [guess, setGuess] = useState(word.split('').map(() => '_'));
   const [letters, setLetters] = useState<Array<String>>([]);
@@ -27,6 +29,7 @@ function App() {
   });
 
   const checkForLetter = (letter: string) => {
+    setTurn(turn + 1);
     let check = false;
     let temp = guess.slice();
     for (let i = 0; i < letters.length; i++) {
@@ -49,74 +52,76 @@ function App() {
 
   if (guess.join('') === word.toUpperCase() || guess.join('') === word.toLowerCase()) {
     return (
-      <div className="End">
+      <div>
         <header className="App-header">
-          <p>Félicitations, vous avez gagné !</p>
-          <p>Il vous restait encore {count} échecs possibles</p>
-          <p>Le mot à trouver était: {word}</p>
-          <button onClick={() => { window.location.reload() }}>Rejouer</button>
+          <p> Jeu du Pendu</p>
         </header>
+        <body className="End">
+          <p>Félicitations, vous avez gagné !</p>
+          <p>Il vous restait encore {count} échecs possibles et vous avez pris {turn} tours pour trouver le mot</p>
+          <p>Le mot à trouver était: {word}</p>
+          <button className="Keyboard" onClick={() => { window.location.reload() }}>Rejouer</button>
+        </body>
+        <footer className='App-footer'>
+          <p>Créé par Damien Birembaut</p>
+        </footer>
       </div>
     );
   }
 
   if (count === 0) {
     return (
-      <div className="End">
+      <div>
         <header className="App-header">
+          <p> Jeu du Pendu</p>
+        </header>
+        <body className='End'>
           <p>Perdu !</p>
           <p>Le mot à trouver était: {word}</p>
-          <button onClick={() => { window.location.reload() }}>Rejouer</button>
-        </header>
+          <button className="Keyboard" onClick={() => { window.location.reload() }}>Rejouer</button>
+        </body>
+        <footer className='App-footer'>
+          <p>Créé par Damien Birembaut</p>
+        </footer>
       </div>
     );
   }
 
   return (
-    <div className="App">
+    <div>
       <header className="App-header">
+        <p> Jeu du Pendu</p>
+      </header>
+      <body className="App">
+        <h1 className='Turns'>Tours: {turn} </h1>
         <p>
           {guess.join(' ')}
           <br />
           Vous avez le droit à: {count} erreurs
         </p>
         <div>
-        <button className='Keyboard' onClick={() => { checkForLetter('A') }}> A </button>
-        <button className='Keyboard' onClick={() => { checkForLetter('Z') }}> Z </button>
-        <button className='Keyboard' onClick={() => { checkForLetter('E') }}> E </button>
-        <button className='Keyboard' onClick={() => { checkForLetter('R') }}> R </button>
-        <button className='Keyboard' onClick={() => { checkForLetter('T') }}> T </button>
-        <button className='Keyboard' onClick={() => { checkForLetter('Y') }}> Y </button>
-        <button className='Keyboard' onClick={() => { checkForLetter('U') }}> U </button>
-        <button className='Keyboard' onClick={() => { checkForLetter('I') }}> I </button>
-        <button className='Keyboard' onClick={() => { checkForLetter('O') }}> O </button>
-        <button className='Keyboard' onClick={() => { checkForLetter('P') }}> P </button>
+        { ["A", "Z", "E", "R", "T", "Y", "U", "I", "O", "P"].map((letter) => { 
+          return <button className="Keyboard" onClick={() => { checkForLetter(letter) }} disabled={letters.includes(letter)}>{letter}</button>
+          })}
         </div>
         <div>
-        <button className='Keyboard' onClick={() => { checkForLetter('Q') }}> Q </button>
-        <button className='Keyboard' onClick={() => { checkForLetter('S') }}> S </button>
-        <button className='Keyboard' onClick={() => { checkForLetter('D') }}> D </button>
-        <button className='Keyboard' onClick={() => { checkForLetter('F') }}> F </button>
-        <button className='Keyboard' onClick={() => { checkForLetter('G') }}> G </button>
-        <button className='Keyboard' onClick={() => { checkForLetter('H') }}> H </button>
-        <button className='Keyboard' onClick={() => { checkForLetter('J') }}> J </button>
-        <button className='Keyboard' onClick={() => { checkForLetter('K') }}> K </button>
-        <button className='Keyboard' onClick={() => { checkForLetter('L') }}> L </button>
-        <button className='Keyboard' onClick={() => { checkForLetter('M') }}> M </button>
+        { ["Q", "S", "D", "F", "G", "H", "J", "K", "L", "M"].map((letter) => {
+          return <button className="Keyboard" onClick={() => { checkForLetter(letter) }} disabled={letters.includes(letter)}>{letter}</button>
+          })}
       </div>
       <div>
-        <button className='Keyboard' onClick={() => { checkForLetter('W') }}> W </button>
-        <button className='Keyboard' onClick={() => { checkForLetter('X') }}> X </button>
-        <button className='Keyboard' onClick={() => { checkForLetter('C') }}> C </button>
-        <button className='Keyboard' onClick={() => { checkForLetter('V') }}> V </button>
-        <button className='Keyboard' onClick={() => { checkForLetter('B') }}> B </button>
-        <button className='Keyboard' onClick={() => { checkForLetter('N') }}> N </button>
+        { ["W", "X", "C", "V", "B", "N"].map((letter) => {
+          return <button className="Keyboard" onClick={() => { checkForLetter(letter) }} disabled={letters.includes(letter)}>{letter}</button>
+          })}
       </div>
       <p>
         {letters.join(', ')}
       </p>
-      </header>
-      </div>
+    </body>
+    <footer className='App-footer'>
+      <p>Créé par Damien Birembaut</p>
+    </footer>
+    </div>
   );
 }
 
---
+++ pendu/src/Win.tsx
@@ -0,0 +1,23 @@
+/*import count from './App'
+import word from './App'
+
+function Win() {
+    return(
+        <div>
+          <header className="App-header">
+            <p> Jeu du Pendu</p>
+          </header>
+          <body className="End">
+            <p>Félicitations, vous avez gagné !</p>
+            <p>Il vous restait encore {count} échecs possibles</p>
+            <p>Le mot à trouver était: {word}</p>
+            <button className="Keyboard" onClick={() => { window.location.reload() }}>Rejouer</button>
+          </body>
+          <footer className='App-footer'>
+            <p>Créé par Damien Birembaut</p>
+          </footer>
+        </div>
+  );
+}
+*/
+export {}
